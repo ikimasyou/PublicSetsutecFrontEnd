3561e16b1100a4b189050f37489c79b0e5f025d5
zp.li <zp.li@setsu-tech.co.jp>
2025-12-15 14:48:32 +0900

    25.12.15.14

--- src/components/dbManager/BPResume.vue
+++ src/components/dbManager/BPResume.vue
@@ -11,16 +11,22 @@
 
         <div class="resumeTable">
             <el-table :data="tableData" border style="width: 100%">
-                <el-table-column prop="date" label="ファイル名" min-width="60%" />
-                <el-table-column prop="name" label="アップロード日" min-width="30%" />
-                <el-table-column prop="address" label="操作" min-width="10%" />
+                <el-table-column prop="fileName" label="ファイル名"  />
+                <el-table-column prop="updatedAt" label="アップロード日"  />
+                <el-table-column label="操作"  align="left">
+                    <template #default="scope">
+                        <el-button type="danger" size="small" @click="handleDelete(scope.row)">
+                            削除
+                        </el-button>
+                    </template>
+                </el-table-column>
             </el-table>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref, getCurrentInstance } from 'vue'
+import { ref, getCurrentInstance, reactive, onMounted } from 'vue'
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 
@@ -28,7 +34,7 @@ const { proxy } = getCurrentInstance()
 
 // 条件设置
 function beforeUpload(file) {
-    const isXls = file.name.endsWith('.xls')
+    const isXls = file.name.endsWith('.xls') || file.name.endsWith('.xlsx')
     if (!isXls) {
         ElMessage.error('xls形式のみアップロード可能です')
     }
@@ -47,11 +53,40 @@ function uploadFile(option) {
         }
     }).then(() => {
         ElMessage.success('アップロード成功')
+        getTableData()
     }).catch(() => {
         ElMessage.error('アップロード失敗')
     })
 }
 
+// 仅仅负责拿到显示到table的内容
+const tableData = reactive([])
+function getTableData() {
+    axios.get(proxy.$server + "/resume/getResumes", {
+        params: {
+            userId: sessionStorage.getItem("userId")
+        }
+    }).then(res => {
+        console.log(res.data)
+        tableData.length = 0
+        const data = res.data.map(resumeInfo => ({
+            fileName: resumeInfo.filepath.split(/[/\\]/).pop(),
+            updatedAt: resumeInfo.updatedAt
+        }))
+        Object.assign(tableData, data)
+    }).catch(eror => console.log(eror))
+}
+//负责删除当前行的内容
+function handleDelete(row){
+    axios.delete(proxy.$server+"/resume/deleteResume",{params:{
+        userId:sessionStorage.getItem("userId"),
+        fileName:row.fileName
+    }}).then(res=>{
+        console.log(res.data)
+        getTableData()
+    }).catch(error=>console.log(error))
+}
+onMounted(() => getTableData())
 </script>
 
 <style scoped>
